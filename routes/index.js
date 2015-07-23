c3ed21bb005b325aedbc466094ab9adc5a807160
Ángel Serrano <angel.serrano@uah.es>
2015-07-23 20:22:29 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/', function(req, res) {
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load);	// autoload:quizId
+router.param('quizId', quizController.load);  // autoload:quizId
+router.param('commentId', commentController.load);  // autoload:commentId
 
 // Definición de rutas de sesión
 router.get('/login',  sessionController.new);     // formulario login
@@ -32,7 +33,9 @@ router.get('/author', function(req, res) {
   res.render('author', {errors:[]});
 });
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',     commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',  sessionController.loginRequierd, commentController.publish);
 
 module.exports = router;
